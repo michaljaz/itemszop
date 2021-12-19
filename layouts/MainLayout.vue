ed818de45b46845f19d172a010ca4c222841645d
Michał Jażdżyk <michal.jazdzyk.007@gmail.com>
2021-12-19 12:34:28 +0100

    navigation drawer

--- layouts/MainLayout.vue
+++ layouts/MainLayout.vue
@@ -6,7 +6,7 @@
         elevation="4"
       >
         <v-container class="pa-0 fill-height justify-space-between">
-          <v-app-bar-nav-icon class="hidden-md-and-up" />
+          <v-app-bar-nav-icon class="hidden-md-and-up" @click.stop="drawer = !drawer" />
           <v-toolbar-title style="cursor: pointer" @click="$router.push('/')">
             ItemSzop
           </v-toolbar-title>
@@ -42,6 +42,28 @@
       <v-container>
         <Nuxt />
       </v-container>
+      <v-navigation-drawer
+        v-model="drawer"
+        absolute
+        temporary
+      >
+        <v-list-item>
+          <v-list-item-avatar>
+            <v-img src="/favicon.ico" />
+          </v-list-item-avatar>
+
+          <v-list-item-content>
+            <v-list-item-title>ItemSzop</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-divider />
+        <v-list-item link to="/auth/signin">
+          Zaloguj się
+        </v-list-item>
+        <v-list-item link to="/auth/signup">
+          Zarejestruj się
+        </v-list-item>
+      </v-navigation-drawer>
     </v-main>
     <v-footer>
       <span>&copy; {{ new Date().getFullYear() }} ItemSzop</span>
@@ -51,6 +73,11 @@
 
 <script>
 export default {
-  name: 'MainLayout'
+  name: 'MainLayout',
+  data () {
+    return {
+      drawer: false
+    }
+  }
 }
 </script>
