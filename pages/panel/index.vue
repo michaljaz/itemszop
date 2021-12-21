4288d1f646a59f886d815a81e640933ba10a675a
Michał Jażdżyk <michal.jazdzyk.007@gmail.com>
2021-12-21 09:28:44 +0100

    shops dump firebase

--- pages/panel/index.vue
+++ pages/panel/index.vue
@@ -1,12 +1,35 @@
 <template>
   <div>
-    Panel
+    <v-container>
+      <v-btn
+        v-for="item in shops"
+        :key="item"
+      >
+        {{ item }}
+      </v-btn>
+    </v-container>
   </div>
 </template>
 
 <script>
 export default {
   name: 'PanelPage',
-  layout: 'PanelLayout'
+  layout: 'PanelLayout',
+  data () {
+    return {
+      shops: []
+    }
+  },
+  mounted () {
+    const { uid } = this.$fire.auth.currentUser
+    const ref = this.$fire.database.ref(`users/${uid}`)
+    ref.get().then((s) => {
+      const shops = s.val()
+      console.log(shops)
+      if (shops) {
+        this.shops = Object.keys(shops)
+      }
+    })
+  }
 }
 </script>
