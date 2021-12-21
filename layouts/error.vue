c4221f9dfa66c61de203e7ddd0d1b12c3579014a
Michał Jażdżyk <michal.jazdzyk.007@gmail.com>
2021-12-21 14:36:05 +0100

    fix issue with drawer shop panel

--- layouts/error.vue
+++ layouts/error.vue
@@ -28,11 +28,8 @@ export default {
     }
   },
   head () {
-    const title =
-      this.error.statusCode === 404 ? this.pageNotFound : this.otherError
-    return {
-      title
-    }
+    const title = this.error.statusCode === 404 ? this.pageNotFound : this.otherError
+    return { title }
   }
 }
 </script>
