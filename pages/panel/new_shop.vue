03caaec0a5cc8e0995f90c4012f0f841dd1f7d2f
Michał Jażdżyk <michal.jazdzyk.007@gmail.com>
2021-12-22 10:30:38 +0100

    new_shop page prototype

--- pages/panel/new_shop.vue
+++ pages/panel/new_shop.vue
@@ -1,5 +1,16 @@
 <template>
-  <div />
+  <div>
+    <v-container>
+      <div class="display-2">
+        Nowy sklep
+      </div>
+      <v-text-field label="Nazwa sklepu" />
+      <v-text-field label="Id sklepu" />
+      <v-btn color="green" text>
+        Dalej
+      </v-btn>
+    </v-container>
+  </div>
 </template>
 <script>
 export default {
