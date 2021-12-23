c963df8f9326610dac306f71697ffbe86d2ef4e8
Michał Jażdżyk <michal.jazdzyk.007@gmail.com>
2021-12-23 16:09:04 +0100

    servers list

--- pages/panel/shop/_shopid/servers.vue
+++ pages/panel/shop/_shopid/servers.vue
@@ -1,8 +1,27 @@
 <template>
-  <div />
+  <div>
+    <v-btn
+      v-for="item in shop.servers"
+      :key="item.name"
+      large
+      block
+      text
+      class="mb-2"
+    >
+      {{ item.name }}
+    </v-btn>
+  </div>
 </template>
 <script>
 export default {
-  name: 'ShopServers'
+  name: 'ShopIndex',
+  props: {
+    shop: {
+      type: Object,
+      default: () => {
+        return {}
+      }
+    }
+  }
 }
 </script>
