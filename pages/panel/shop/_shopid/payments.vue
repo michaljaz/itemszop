94dbc8d9a20239604a18375483123aeade11a98e
Michał Jażdżyk <michal.jazdzyk.007@gmail.com>
2021-12-25 11:24:03 +0100

    payments prototype

--- pages/panel/shop/_shopid/payments.vue
+++ pages/panel/shop/_shopid/payments.vue
@@ -1,8 +1,25 @@
 <template>
-  <div />
+  <div>
+    <v-select
+      v-model="select"
+      :items="items"
+      label="Standard"
+    />
+    <v-text-field label="ID Użytkownika" autocomplete="new-password" />
+    <v-text-field label="Numer ID (Przelew)" autocomplete="new-password" />
+    <v-text-field label="Hash (Przelew)" autocomplete="new-password" />
+    <v-text-field label="Numer ID (SMS)" autocomplete="new-password" />
+    <v-text-field label="Treść SMS" autocomplete="new-password" />
+  </div>
 </template>
 <script>
 export default {
-  name: 'ShopPayments'
+  name: 'ShopPayments',
+  data () {
+    return {
+      select: 'MicroSMS',
+      items: ['MicroSMS']
+    }
+  }
 }
 </script>
