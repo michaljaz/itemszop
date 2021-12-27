31b8716455797a517696485f56b8a9e0614486c8
Michał Jażdżyk <michal.jazdzyk.007@gmail.com>
2021-12-27 21:23:31 +0100

    servers card deck

--- pages/panel/shop/_shopid/servers.vue
+++ pages/panel/shop/_shopid/servers.vue
@@ -4,104 +4,125 @@
       v-model="dialog"
       max-width="600px"
     >
-      <v-card>
+      <v-card elevation="10">
         <v-card-title>
           <span class="text-h5">Zarządzanie serwerem</span>
-          <v-tabs
-            v-model="tab"
-          >
-            <v-tab>
-              Konfiguracja
-            </v-tab>
-            <v-tab>
-              RCON
-            </v-tab>
-          </v-tabs>
         </v-card-title>
         <v-card-text>
-          <v-tabs-items v-model="tab">
-            <v-tab-item>
-              <v-form
-                ref="form"
-                v-model="valid"
-              >
-                <v-text-field
-                  v-model="serverName"
-                  label="Nazwa serwera"
-                  :rules="rulesName"
-                  autocomplete="new-password"
-                />
-                <v-text-field
-                  v-model="serverIp"
-                  label="IP serwera"
-                  :rules="rulesIp"
-                  autocomplete="new-password"
-                />
-                <v-text-field
-                  v-model="serverPort"
-                  label="Port RCON"
-                  :rules="rulesPort"
-                  autocomplete="new-password"
-                  type="number"
-                />
-                <v-text-field
-                  v-model="serverPassword"
-                  autocomplete="new-password"
-                  label="Hasło RCON"
-                  :rules="rulesPassword"
-                  :type="showPassword ? 'text' : 'password'"
-                  :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
-                  @click:append="showPassword = !showPassword"
-                />
-                <v-spacer />
-                <v-btn
-                  color="blue darken-1"
-                  text
-                  @click="dialog = false"
-                >
-                  Anuluj
-                </v-btn>
-                <v-btn
-                  color="green darken-1"
-                  text
-                  @click="saveServer"
-                >
-                  Zapisz
-                </v-btn>
-                <v-btn
-                  color="red darken-1"
-                  text
-                  @click="removeServer"
-                >
-                  Usuń
-                </v-btn>
-              </v-form>
-            </v-tab-item>
-            <v-tab-item>
-              <v-text-field v-model="rconCommand" label="Komenda" autocomplete="new-password" />
-              <v-btn color="blue" @click="runRcon">
-                Wyślij do serwera
-              </v-btn>
-              <div v-html="rconResponse" />
-            </v-tab-item>
-          </v-tabs-items>
+          <v-form
+            ref="form"
+            v-model="valid"
+          >
+            <v-text-field
+              v-model="serverName"
+              label="Nazwa serwera"
+              :rules="rulesName"
+              autocomplete="new-password"
+            />
+            <v-text-field
+              v-model="serverIp"
+              label="IP serwera"
+              :rules="rulesIp"
+              autocomplete="new-password"
+            />
+            <v-text-field
+              v-model="serverPort"
+              label="Port RCON"
+              :rules="rulesPort"
+              autocomplete="new-password"
+              type="number"
+            />
+            <v-text-field
+              v-model="serverPassword"
+              autocomplete="new-password"
+              label="Hasło RCON"
+              :rules="rulesPassword"
+              :type="showPassword ? 'text' : 'password'"
+              :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+              @click:append="showPassword = !showPassword"
+            />
+            <v-spacer />
+            <v-btn
+              color="blue darken-1"
+              text
+              @click="dialog = false"
+            >
+              Anuluj
+            </v-btn>
+            <v-btn
+              color="green darken-1"
+              text
+              @click="saveServer"
+            >
+              Zapisz
+            </v-btn>
+          </v-form>
         </v-card-text>
         <v-card-actions />
       </v-card>
     </v-dialog>
-    <v-btn
-      v-for="item in serversList"
-      :key="item"
-      large
-      block
-      text
-      class="mb-2"
-      @click="serverDialog(item)"
+    <v-dialog
+      v-model="rconDialog"
+      max-width="600px"
     >
-      <span v-if="servers[item]">
-        {{ servers[item].serverName }}
-      </span>
-    </v-btn>
+      <v-card elevation="10">
+        <v-card-title>
+          <span class="text-h5">Konsola RCON</span>
+        </v-card-title>
+        <v-card-text>
+          <v-text-field v-model="rconCommand" label="Komenda" autocomplete="new-password" />
+          <v-btn color="blue" @click="runRcon">
+            Wyślij do serwera
+          </v-btn>
+          <div v-html="rconResponse" />
+        </v-card-text>
+        <v-card-actions />
+      </v-card>
+    </v-dialog>
+    <v-row>
+      <v-col
+        v-for="item in serversList"
+        :key="item"
+        cols="12"
+        lg="4"
+        md="6"
+      >
+        <v-card
+          text
+          class="mb-2"
+          elevation="10"
+        >
+          <v-list-item three-line>
+            <v-list-item-content>
+              <v-list-item-title class="text-h5 mb-1">
+                {{ servers[item].serverName }}
+              </v-list-item-title>
+              <v-list-item-subtitle>
+                <div>
+                  IP serwera: {{ servers[item].serverIp }}
+                </div>
+                <div class="mt-2">
+                  Port RCON: {{ servers[item].serverPort }}
+                </div>
+              </v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+
+          <v-card-actions>
+            <v-spacer />
+            <v-btn text color="green" @click="consoleDialog(item)">
+              Konsola
+            </v-btn>
+            <v-btn text color="blue" @click="editDialog(item)">
+              Edytuj
+            </v-btn>
+            <v-btn text color="red" @click="removeServer(item)">
+              Usuń
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
     <v-btn
       large
       block
@@ -125,9 +146,9 @@ export default {
   },
   data () {
     return {
+      rconDialog: false,
       rconResponse: '',
       rconCommand: '',
-      tab: null,
       valid: false,
       serverPort: '',
       serverName: '',
@@ -190,13 +211,20 @@ export default {
         return []
       }
     },
-    serverDialog (serverId) {
+    applyServer (serverId) {
       const server = this.servers[serverId]
       this.serverId = serverId
       this.serverPort = server.serverPort
       this.serverName = server.serverName
       this.serverIp = server.serverIp
       this.serverPassword = server.serverPassword
+    },
+    consoleDialog (serverId) {
+      this.applyServer(serverId)
+      this.rconDialog = true
+    },
+    editDialog (serverId) {
+      this.applyServer(serverId)
       this.dialog = true
     },
     saveServer () {
@@ -215,9 +243,8 @@ export default {
         this.dialog = false
       }
     },
-    removeServer () {
+    removeServer (serverId) {
       const { shopid } = this.$route.params
-      const { serverId } = this
       delete this.servers[serverId]
       this.serversList = this.getServersList()
       this.$fire.database.ref().child(`shops/${shopid}/servers/${serverId}`).remove()
@@ -233,18 +260,18 @@ export default {
       this.serverId = `server_${(Math.random() + 1).toString(36).substring(7)}`
       this.dialog = true
     },
-    sendRcon (host, port, password, command) {
+    sendRconCommand (host, port, password, command) {
       return this.$axios.get('/rcon', {
         params: { host, port, password, command }
       })
     },
     runRcon () {
-      this.sendRcon(this.serverIp, this.serverPort, this.serverPassword, this.rconCommand).then((response) => {
+      this.sendRconCommand(this.serverIp, this.serverPort, this.serverPassword, this.rconCommand).then((response) => {
         const { data } = response
         if (data.error === 'auth') {
           this.rconResponse = 'Nie udało się połączyć'
         } else {
-          this.rconResponse = require('minecraft-text-js').toHTML(data.response).replaceAll('\n', '<br>')
+          this.rconResponse = require('minecraft-text-js').default.toHTML(data.response).replaceAll('\n', '<br>')
         }
       })
     }
