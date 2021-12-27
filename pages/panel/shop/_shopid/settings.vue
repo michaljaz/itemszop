0d654db2cd0b007f3c6f0cb7f4260e0fe7582c7a
Michał Jażdżyk <michal.jazdzyk.007@gmail.com>
2021-12-27 13:46:19 +0100

    month goal

--- pages/panel/shop/_shopid/settings.vue
+++ pages/panel/shop/_shopid/settings.vue
@@ -1,5 +1,12 @@
 <template>
   <div>
+    <v-text-field label="Nazwa sklepu" />
+    Cel miesięczny {{ goal }} zł
+    <v-slider
+      v-model="goal"
+      min="1"
+      max="500"
+    />
     <v-btn color="red" @click="remove">
       Usuń sklep
     </v-btn>
@@ -8,10 +15,22 @@
 <script>
 export default {
   name: 'ShopSettings',
+  props: {
+    shop: {
+      type: Object,
+      default: () => {
+        return {}
+      }
+    }
+  },
+  data () {
+    return {
+      goal: 0
+    }
+  },
   methods: {
     remove () {
       const { uid } = this.$fire.auth.currentUser
-      // console.log(this.$route.params.shopid)
       this.$fire.database.ref().child(`shops/${this.$route.params.shopid}`).remove().then(() => {
         this.$fire.database.ref().child(`users/${uid}/${this.$route.params.shopid}`).remove()
       })
