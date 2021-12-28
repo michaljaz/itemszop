a8dbb1eb4739668f0c545150fa22671a4e107774
Michał Jażdżyk <michal.jazdzyk.007@gmail.com>
2021-12-28 11:32:51 +0100

    tiptap-vuetify editor

---
+++ components/TiptapEditor.vue
@@ -0,0 +1,76 @@
+<template>
+  <ClientOnly>
+    <!-- Use the component in the right place of the template -->
+    <tiptap-vuetify v-model="content" :extensions="extensions" :toolbar-attributes="toolbarAttrs" />
+
+    <template #placeholder>
+      Loading...
+    </template>
+  </ClientOnly>
+</template>
+
+<script>
+// import the component and the necessary extensions
+import {
+  TiptapVuetify,
+  Heading,
+  Bold,
+  Italic,
+  Strike,
+  Underline,
+  Code,
+  Paragraph,
+  BulletList,
+  OrderedList,
+  ListItem,
+  Link,
+  Blockquote,
+  HardBreak,
+  HorizontalRule,
+  History
+} from 'tiptap-vuetify'
+export default {
+  // specify TiptapVuetify component in "components"
+  components: { TiptapVuetify },
+  data: () => ({
+    // declare extensions you want to use
+    extensions: [
+      History,
+      Blockquote,
+      Link,
+      Underline,
+      Strike,
+      Italic,
+      ListItem,
+      BulletList,
+      OrderedList,
+      [
+        Heading,
+        {
+          options: {
+            levels: [1, 2, 3]
+          }
+        }
+      ],
+      Bold,
+      Link,
+      Code,
+      HorizontalRule,
+      Paragraph,
+      HardBreak
+    ],
+    // starting editor's content
+    content: `
+      <h1>Yay Headlines!</h1>
+      <p>All these <strong>cool tags</strong> are working now.</p>
+    `
+  }),
+  computed: {
+    toolbarAttrs () {
+      return this.$vuetify.theme.isDark
+        ? { color: 'black', dark: false }
+        : { color: 'yellow' }
+    }
+  }
+}
+</script>
