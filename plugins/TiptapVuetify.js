ef14a374aab42776265d92d9b63843b684d7d828
Michał Jażdżyk <michal.jazdzyk.007@gmail.com>
2021-12-28 20:59:42 +0100

    test discord webhook

--- plugins/TiptapVuetify.js
+++ plugins/TiptapVuetify.js
@@ -4,7 +4,6 @@ import 'tiptap-vuetify/dist/main.css'
 
 export default ({ app }) => {
   Vue.use(TiptapVuetifyPlugin, {
-    // Below is an IMPORTANT line.
     vuetify: app.vuetify,
     iconsGroup: 'mdi'
   })
