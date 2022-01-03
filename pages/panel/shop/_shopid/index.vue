e82b61db7b652b6ee0a4d94d3474d43e2b2d7568
Michał Jażdżyk <michal.jazdzyk.007@gmail.com>
2022-01-03 10:14:01 +0100

    notification shop url

--- pages/panel/shop/_shopid/index.vue
+++ pages/panel/shop/_shopid/index.vue
@@ -1,6 +1,13 @@
 <template>
   <div>
-    {{ shop.name }}
+    <v-alert
+      border="top"
+      colored-border
+      type="info"
+      elevation="2"
+    >
+      Twój sklep jest dostępny pod adresem <a :href="url">{{ url }}</a>
+    </v-alert>
   </div>
 </template>
 <script>
@@ -10,6 +17,11 @@ export default {
     shop: {
       type: Object,
       required: true
+    }
+  },
+  data () {
+    return {
+      url: `${document.location.origin}/shop/${this.$route.params.shopid}`
     }
   },
   head () {
