bc67a1d8913fc222a73a5e55986b676ad65e4c37
Michał Jażdżyk <michal.jazdzyk.007@gmail.com>
2022-01-04 09:55:44 +0100

    services deck

--- pages/shop/_shopid/server/_serverid.vue
+++ pages/shop/_shopid/server/_serverid.vue
@@ -1,5 +1,50 @@
 <template>
-  <div />
+  <div>
+    <v-row>
+      <v-col v-for="service in services" :key="service.serviceId" cols="12" md="4">
+        <v-card elevation="5" height="100%">
+          <center>
+            <template v-if="service.icon">
+              <v-img :src="service.iconUrl" height="120" width="120" />
+            </template>
+            <template v-else>
+              <v-img height="120" width="120" />
+            </template>
+          </center>
+          <v-card-text>
+            <v-row>
+              <v-col cols="12" xs="6" md="6" class="pb-0">
+                SMS
+              </v-col>
+              <v-col cols="12" xs="6" md="6" class="text-right pb-0">
+                <template v-if="service.sms">
+                  0
+                </template>
+                <template v-else>
+                  X
+                </template>
+              </v-col>
+              <v-col cols="12" xs="6" md="6" class="pt-1">
+                Przelew
+              </v-col>
+              <v-col cols="12" xs="6" md="6" class="text-right pt-1">
+                <template v-if="service.przelew">
+                  {{ service.przelewCost }}
+                </template>
+                <template v-else>
+                  X
+                </template>
+              </v-col>
+            </v-row>
+          </v-card-text>
+          <v-divider class="mx-4" />
+          <v-card-title class="justify-center">
+            {{ service.name }}
+          </v-card-title>
+        </v-card>
+      </v-col>
+    </v-row>
+  </div>
 </template>
 <script>
 export default {
@@ -29,7 +74,9 @@ export default {
       const { serverid } = this.$route.params
       for (const serviceId in this.shop.services) {
         if (this.shop.services[serviceId].server === serverid) {
-          result.push(this.shop.services[serviceId])
+          const service = Object.assign({}, this.shop.services[serviceId])
+          service.serviceId = serviceId
+          result.push(service)
         }
       }
       this.services = result
