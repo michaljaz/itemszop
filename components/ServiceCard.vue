346cf16bf90474161233e1191c2705a5d05ecf8f
Michał Jażdżyk <michal.jazdzyk.007@gmail.com>
2022-01-07 12:30:27 +0100

    payment url generation

--- components/ServiceCard.vue
+++ components/ServiceCard.vue
@@ -1,105 +1,111 @@
 <template>
-  <v-card elevation="5" height="100%" :to="link">
-    <center>
-      <template v-if="service.icon">
-        <v-img :src="service.iconUrl" max-height="120" contain />
-      </template>
-      <template v-else>
-        <v-img src="/item.png" max-height="120" contain />
-      </template>
-    </center>
-    <v-card-text>
-      <v-row>
-        <v-col cols="12" xs="6" sm="6" md="6" class="pb-0">
-          SMS
-        </v-col>
-        <v-col cols="12" xs="6" sm="6" md="6" class="text-right pb-0">
-          <template v-if="service.sms">
-            {{ smsCost[service.smsType] }} zł
-          </template>
-          <template v-else>
-            X
-          </template>
-        </v-col>
-        <v-col cols="12" xs="6" sm="6" md="6" class="pt-1">
-          Przelew
-        </v-col>
-        <v-col cols="12" xs="6" sm="6" md="6" class="text-right pt-1">
-          <template v-if="service.przelew">
-            {{ service.przelewCost }} zł
-          </template>
-          <template v-else>
-            X
-          </template>
-        </v-col>
-      </v-row>
-    </v-card-text>
-    <v-divider class="mx-4" />
-    <v-card-title class="justify-center">
-      {{ service.name }}
-    </v-card-title>
-    <v-card-actions v-if="!link">
-      <v-dialog
-        v-model="dialog"
-        width="500"
-      >
-        <template #activator="{ on, attrs }">
-          <v-btn
-            color="green"
-            large
-            outlined
-            block
-            v-bind="attrs"
-            v-on="on"
-          >
-            Kupuję
-          </v-btn>
+  <div>
+    <v-card elevation="5" height="100%" :to="link">
+      <center>
+        <template v-if="service.icon">
+          <v-img :src="service.iconUrl" max-height="120" contain />
         </template>
-
-        <v-card elevation="5" outlined>
-          <v-card-title class="headline">
-            {{ service.name }}
-          </v-card-title>
-
-          <v-card-text>
-            <v-btn-toggle
-              v-model="text"
-              tile
-              color="blue"
-              group
-            >
-              <v-btn v-if="service.przelew" value="przelew">
-                Przelew
-              </v-btn>
-
-              <v-btn v-if="service.sms" value="sms">
-                SMS
-              </v-btn>
-            </v-btn-toggle>
-            <v-text-field label="Wprowadź swój nick" />
-          </v-card-text>
-
-          <v-divider />
-
-          <v-card-actions>
-            <v-spacer />
+        <template v-else>
+          <v-img src="/item.png" max-height="120" contain />
+        </template>
+      </center>
+      <v-card-text>
+        <v-row>
+          <v-col cols="12" xs="6" sm="6" md="6" class="pb-0">
+            SMS
+          </v-col>
+          <v-col cols="12" xs="6" sm="6" md="6" class="text-right pb-0">
+            <template v-if="service.sms">
+              {{ smsCost[service.smsType] }} zł
+            </template>
+            <template v-else>
+              X
+            </template>
+          </v-col>
+          <v-col cols="12" xs="6" sm="6" md="6" class="pt-1">
+            Przelew
+          </v-col>
+          <v-col cols="12" xs="6" sm="6" md="6" class="text-right pt-1">
+            <template v-if="service.przelew">
+              {{ service.przelewCost }} zł
+            </template>
+            <template v-else>
+              X
+            </template>
+          </v-col>
+        </v-row>
+      </v-card-text>
+      <v-divider class="mx-4" />
+      <v-card-title class="justify-center">
+        {{ service.name }}
+      </v-card-title>
+      <v-card-actions v-if="!link">
+        <v-dialog
+          v-model="dialog"
+          width="500"
+        >
+          <template #activator="{ on, attrs }">
             <v-btn
-              color="primary"
-              text
-              @click="dialog = false"
+              color="green"
+              large
+              outlined
+              block
+              v-bind="attrs"
+              v-on="on"
             >
-              I accept
+              Kupuję
             </v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-dialog>
-    </v-card-actions>
-  </v-card>
+          </template>
+
+          <v-card elevation="5" outlined>
+            <v-card-title class="headline">
+              {{ service.name }}
+            </v-card-title>
+
+            <v-card-text>
+              <v-btn-toggle
+                v-model="type"
+                tile
+                color="blue"
+                group
+              >
+                <v-btn v-if="service.przelew" value="przelew">
+                  Przelew
+                </v-btn>
+
+                <v-btn v-if="service.sms" value="sms">
+                  SMS
+                </v-btn>
+              </v-btn-toggle>
+              <v-text-field v-model="nick" label="Wprowadź swój nick" />
+            </v-card-text>
+
+            <v-divider />
+
+            <v-card-actions>
+              <v-spacer />
+              <v-btn
+                color="primary"
+                text
+                @click="next"
+              >
+                Dalej
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+      </v-card-actions>
+    </v-card>
+  </div>
 </template>
 <script>
 export default {
   name: 'ServiceCard',
   props: {
+    payments: {
+      type: Object,
+      default: () => ({})
+    },
     service: {
       type: Object,
       required: true
@@ -111,7 +117,8 @@ export default {
   },
   data () {
     return {
-      text: '',
+      nick: '',
+      type: '',
       dialog: false,
       smsCost: {
         1: '1.23',
@@ -126,6 +133,35 @@ export default {
         10: '24.60',
         11: '30.75'
       }
+    }
+  },
+  methods: {
+    next () {
+      if (this.type === 'przelew') {
+        this.buyPrzelew()
+      } else {
+        this.buySMS()
+      }
+    },
+    md5 (msg) {
+      return require('md5')(msg)
+    },
+    buyPrzelew () {
+      const data = {
+        shopid: this.payments.paymentsPrzelewId,
+        amount: this.service.przelewCost,
+        signature: this.md5(`${this.payments.paymentsPrzelewId}${this.payments.paymentsHash}${this.service.przelewCost}`),
+        description: `${this.service.name} dla ${this.nick}`,
+        control: `${this.nick}|${this.service.serviceId}`,
+        returl_url: 'https://itemszop.vercel.app',
+        returl_urlc: 'https://itemszop.vercel.app'
+      }
+      const params = new URLSearchParams(data)
+      const url = `https://microsms.pl/api/bankTransfer/?${params}`
+      document.location.href = url
+    },
+    buySMS () {
+
     }
   }
 }
--- pages/shop/_shopid/service/_serviceid.vue
+++ pages/shop/_shopid/service/_serviceid.vue
@@ -2,7 +2,7 @@
   <div>
     <v-row>
       <v-col cols="12" md="5" sm="6">
-        <ServiceCard :service="service" />
+        <ServiceCard :service="service" :payments="shop.payments" />
       </v-col>
       <v-col cols="12" md="7" sm="6">
         <!-- eslint-disable vue/no-v-html -->
