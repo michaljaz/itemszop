45933968c191000bc95ef4c14657c746f8c50c04
Michał Jażdżyk <michal.jazdzyk.007@gmail.com>
2022-01-09 09:57:56 +0100

    firebase service account in serverless

--- api/voucher.js
+++ api/voucher.js
@@ -1,8 +1,32 @@
 const app = require('express')()
 const cors = require('cors')
+import * as admin from "firebase-admin"
+
+if(process.env.FIREBASE_SERVICE_ACCOUNT_KEY===undefined){
+  console.log("FIREBASE_SERVICE_ACCOUNT_KEY is not set")
+}else{
+  const buff = new Buffer.from(process.env.FIREBASE_SERVICE_ACCOUNT_KEY, 'base64')
+  const text = buff.toString('ascii')
+  const serviceAccount = JSON.parse(text)
+  if (admin.apps.length === 0) {
+    admin.initializeApp({
+      credential: admin.credential.cert(serviceAccount),
+      databaseURL: process.env.FIREBASE_DATABASE_URL,
+    })
+  }
+}
+
 app.use(cors())
 app.get('/api/voucher', async (req, res) => {
-  res.send("OK")
+  try{
+    admin.database().ref().child('transactions')
+    .once('value',(snapshot)=>{
+      const transactions=snapshot.val()
+      res.json({transactions,success:true})
+    })
+  }catch(e){
+    res.json({success:false})
+  }
 })
 
 module.exports = app
--- nuxt.config.js
+++ nuxt.config.js
@@ -42,6 +42,7 @@ export default {
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
+    '@nuxtjs/dotenv',
     // https://go.nuxtjs.dev/eslint
     ['@nuxtjs/eslint-module', {
       fix: true
