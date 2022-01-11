dff80d317f70227c53f00a65a71ebac1398342e6
Michał Jażdżyk <michal.jazdzyk.007@gmail.com>
2022-01-11 19:37:08 +0100

    ready to check voucher

--- pages/shop/_shopid/voucher.vue
+++ pages/shop/_shopid/voucher.vue
@@ -4,20 +4,25 @@
       <v-col cols="12" sm="8" md="6">
         <center class="mt-5">
           <h1>Sprawdź voucher</h1>
-          <v-text-field
-            v-model="code"
-            class="mt-5"
-            label="Kod"
-            prepend-icon="mdi-ticket-percent"
-            :rules="rules.code"
-          />
-          <v-text-field
-            v-model="nick"
-            class="mt-5"
-            label="Nick"
-            prepend-icon="mdi-account"
-            :rules="rules.nick"
-          />
+          <v-form
+            ref="form"
+            v-model="valid"
+          >
+            <v-text-field
+              v-model="code"
+              class="mt-5"
+              label="Kod"
+              prepend-icon="mdi-ticket-percent"
+              :rules="rules.code"
+            />
+            <v-text-field
+              v-model="nick"
+              class="mt-5"
+              label="Nick"
+              prepend-icon="mdi-account"
+              :rules="rules.nick"
+            />
+          </v-form>
           <v-btn class="blue darken-4" large @click="check">
             Sprawdź
           </v-btn>
@@ -37,12 +42,13 @@ export default {
   },
   data () {
     return {
+      valid: false,
       code: '',
       nick: '',
       rules: {
         code: [
           value => !!value || 'Wpisz kod',
-          value => (value && value.length === 11) || 'Kod vouchera musi zawierać 11 znaków'
+          v => /^[a-b0-9]{11}$/.test(v) || 'Nieprawidłowy format'
         ],
         nick: [
           value => !!value || 'Wpisz nick'
@@ -63,8 +69,16 @@ export default {
   },
   methods: {
     check () {
-      const { code, nick } = this
-      console.log(code, nick)
+      this.$refs.form.validate()
+      if (this.valid) {
+        const { code, nick } = this
+        const { shopid } = this.$route.params
+        this.$axios.get('/voucher', {
+          params: { code, nick, shopid }
+        }).then(({ data }) => {
+          console.log(data)
+        })
+      }
     }
   }
 }
