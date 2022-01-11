b870711810e0b6f6d3f5b2ea2f0276a5cccc87db
Michał Jażdżyk <michal.jazdzyk.007@gmail.com>
2022-01-11 19:28:33 +0100

    prepare voucher functions

--- pages/shop/_shopid/voucher.vue
+++ pages/shop/_shopid/voucher.vue
@@ -4,9 +4,21 @@
       <v-col cols="12" sm="8" md="6">
         <center class="mt-5">
           <h1>Sprawdź voucher</h1>
-          <v-text-field class="mt-5" label="Kod" prepend-icon="mdi-ticket-percent" />
-          <v-text-field class="mt-5" label="Nick" prepend-icon="mdi-account" />
-          <v-btn class="blue darken-4" large>
+          <v-text-field
+            v-model="code"
+            class="mt-5"
+            label="Kod"
+            prepend-icon="mdi-ticket-percent"
+            :rules="rules.code"
+          />
+          <v-text-field
+            v-model="nick"
+            class="mt-5"
+            label="Nick"
+            prepend-icon="mdi-account"
+            :rules="rules.nick"
+          />
+          <v-btn class="blue darken-4" large @click="check">
             Sprawdź
           </v-btn>
         </center>
@@ -23,6 +35,21 @@ export default {
       required: true
     }
   },
+  data () {
+    return {
+      code: '',
+      nick: '',
+      rules: {
+        code: [
+          value => !!value || 'Wpisz kod',
+          value => (value && value.length === 11) || 'Kod vouchera musi zawierać 11 znaków'
+        ],
+        nick: [
+          value => !!value || 'Wpisz nick'
+        ]
+      }
+    }
+  },
   mounted () {
     this.$emit('breadcrumb', [{
       text: 'Sklep',
@@ -33,6 +60,12 @@ export default {
       text: 'Sprawdź voucher',
       disabled: true
     }])
+  },
+  methods: {
+    check () {
+      const { code, nick } = this
+      console.log(code, nick)
+    }
   }
 }
 </script>
