990ed9cbe21956e44f164bd9ad906d49d5c3b161
Michał Jażdżyk <michal.jazdzyk.007@gmail.com>
2022-01-12 18:28:46 +0100

    footer links

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,7 +4,12 @@
       <Nuxt />
     </v-main>
     <v-footer class="justify-center">
-      <span>&copy; {{ new Date().getFullYear() }} ItemSzop</span>
+      <span>
+        &copy; {{ new Date().getFullYear() }}
+        <a href="https://itemszop.vercel.app">ItemSzop</a>
+        by
+        <a href="https://github.com/michaljaz">michaljaz</a>
+      </span>
     </v-footer>
   </v-app>
 </template>
