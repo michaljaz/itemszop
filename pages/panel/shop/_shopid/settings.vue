a796776dbe955634a6f36fe15968738fc192f378
Michał Jażdżyk <michal.jazdzyk.007@gmail.com>
2022-01-28 16:23:58 +0100

    remove shop with confirm message

--- pages/panel/shop/_shopid/settings.vue
+++ pages/panel/shop/_shopid/settings.vue
@@ -16,17 +16,54 @@
             />
           </v-card-text>
           <v-card-actions>
-            <v-spacer />
             <v-btn color="green" rounded text @click="save">
               Zapisz
             </v-btn>
-            <v-btn color="red" rounded text @click="remove">
+          </v-card-actions>
+        </v-card>
+        <v-card class="pt-1 mt-5 pb-4" elevation="10">
+          <v-card-title class="headline">
+            Ustawienia zaawansowane
+          </v-card-title>
+          <v-card-text>
+            <v-btn color="red" @click="removeDialog">
               Usuń sklep
             </v-btn>
-          </v-card-actions>
+          </v-card-text>
         </v-card>
       </v-col>
     </v-row>
+    <v-dialog
+      v-model="dialog"
+      max-width="400"
+    >
+      <v-card>
+        <v-card-title class="text-h5">
+          Czy jesteś tego pewien?
+        </v-card-title>
+        <v-card-text>
+          <v-alert
+            border="left"
+            color="red"
+            dark
+          >
+            Jeśli tego nie przeczytasz staną się nieoczekiwane złe rzeczy!
+          </v-alert>
+          Tej czynności nie można cofnąć.
+          Spowoduje to trwałe usunięcie sklepu serwera oraz wszystkich rzeczy w tym sklepie takich jak Serwerów, Usług, Voucherów, Płatności.
+          <br>
+          <div class="pt-2">
+            Wpisz <strong>{{ $route.params.shopid }}</strong> aby potwierdzić swoją operację:
+            <v-text-field v-model="cdel" />
+          </div>
+        </v-card-text>
+        <v-card-actions>
+          <v-btn block color="red" :disabled="cdel===$route.params.shopid ? false : true" @click="remove()">
+            Rozumiem konsekwencje, usuwam sklep
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </div>
 </template>
 <script>
@@ -40,8 +77,10 @@ export default {
   },
   data () {
     return {
+      cdel: '',
       goal: this.shop.goal,
-      name: this.shop.name
+      name: this.shop.name,
+      dialog: false
     }
   },
   head () {
@@ -56,6 +95,9 @@ export default {
         name: this.name,
         goal: this.goal
       })
+    },
+    removeDialog () {
+      this.dialog = true
     },
     remove () {
       if (this.shop.servers) {
