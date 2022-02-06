d09884536c1d5195aa7ac7fbc7962dcb6a7ca650
Michał Jażdżyk <michal.jazdzyk.007@gmail.com>
2022-02-06 21:54:15 +0100

    select chart type in panel

--- pages/panel/shop/_shopid/index.vue
+++ pages/panel/shop/_shopid/index.vue
@@ -5,41 +5,36 @@
     </h1>
     <v-row>
       <v-col cols="12" md="6">
-        <v-card class="mb-3">
-          <v-card-title>
-            {{ $t('titles.payment_methods') }}
-          </v-card-title>
-          <v-card-text>
-            <PieChart :data="pieChartData" :options="pieChartOptions" :height="200" />
-          </v-card-text>
-        </v-card>
-      </v-col>
-      <v-col cols="12" md="6">
-        <v-card class="mb-3">
-          <v-card-title>
-            {{ $t('titles.most_common_services') }}
-          </v-card-title>
-          <v-card-text>
-            <PieChart :data="pieChartData2" :options="pieChartOptions2" :height="200" />
-          </v-card-text>
-        </v-card>
-      </v-col>
-    </v-row>
-    <v-alert
-      border="top"
-      colored-border
-      type="info"
-      elevation="2"
-    >
-      {{ $t('misc.shop_url') }} <a :href="url">{{ url }}</a>
-    </v-alert>
-    <v-row>
-      <v-col cols="12" md="6">
+        <v-alert
+          border="top"
+          colored-border
+          type="info"
+          elevation="2"
+        >
+          {{ $t('misc.shop_url') }} <a :href="url">{{ url }}</a>
+        </v-alert>
         <v-textarea
           :label="$t('fields.shop_html')"
           filled
           :value="`<iframe style=&quot;z-index:99999999;border:0px;position:fixed;width:100%;height:100%;top:0px;left:0px;&quot; src=&quot;${url}&quot; />`"
         />
+      </v-col>
+      <v-col cols="12" md="6">
+        <v-card class="mb-3">
+          <v-card-title>
+            <v-select
+              v-model="selectedChart"
+              :items="charts"
+              item-text="name"
+              item-value="id"
+              solo
+            />
+          </v-card-title>
+          <v-card-text>
+            <PieChart :data="pieChartData" :options="pieChartOptions" :height="200" />
+          </v-card-text>
+          </v-card-text>
+        </v-card>
       </v-col>
     </v-row>
   </div>
@@ -56,16 +51,28 @@ export default {
   },
   data () {
     return {
+      selectedChart: 1,
+      charts: [
+        { name: this.$t('titles.payment_methods'), id: 1 },
+        { name: this.$t('titles.most_common_services'), id: 2 }
+      ],
       url: `${document.location.origin}/shop/${this.$route.params.shopid}`,
       pieChartData: this.getData1(),
-      pieChartOptions: {},
-      pieChartData2: this.getData2(),
-      pieChartOptions2: {}
+      pieChartOptions: {}
     }
   },
   head () {
     return {
       title: this.$t('titles.dashboard')
+    }
+  },
+  watch: {
+    selectedChart (newData, oldData) {
+      if (newData === 1) {
+        this.pieChartData = this.getData1()
+      } else {
+        this.pieChartData = this.getData2()
+      }
     }
   },
   methods: {
@@ -98,11 +105,13 @@ export default {
           label: 'My First Dataset',
           data,
           backgroundColor: [
-            '#52b69a',
-            '#76c893',
-            '#99d98c',
-            '#b5e48c',
-            '#d9ed92'
+            '#005f73',
+            '#0a9396',
+            '#94d2bd',
+            '#e9d8a6',
+            '#ee9b00',
+            '#ca6702',
+            '#bb3e03'
           ],
           borderColor: 'rgba(0,0,0,0)',
           hoverOffset: 4
