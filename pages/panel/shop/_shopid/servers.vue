5a43d313afb836c76de28da18f6be83e2c03726b
Michał Jażdżyk <michal.jazdzyk.007@gmail.com>
2022-02-22 18:02:26 +0100

    servers as vuetify table

--- pages/panel/shop/_shopid/servers.vue
+++ pages/panel/shop/_shopid/servers.vue
@@ -1,155 +1,153 @@
 <template>
   <div>
-    <v-dialog
-      v-model="dialog"
-      fullscreen
-      hide-overlay
-      scrollable
+    <v-data-table
+      :headers="data_headers"
+      :items="serversList"
+      :items-per-page="5"
+      class="elevation-1"
     >
-      <v-card tile flat>
+      <template #top>
         <v-toolbar
-          max-height="80"
-          dark
-          color="primary"
-          class="mb-4"
+          flat
         >
-          <v-btn
-            icon
-            dark
-            @click="dialog = false"
-          >
-            <v-icon>mdi-close</v-icon>
-          </v-btn>
-          <v-toolbar-title>
-            <span class="text-h5">{{ $t('titles.server_config') }}</span>
-          </v-toolbar-title>
+          <v-toolbar-title>{{ $t('titles.servers') }}</v-toolbar-title>
+          <v-divider
+            class="mx-4"
+            inset
+            vertical
+          />
           <v-spacer />
-          <v-toolbar-items>
-            <v-btn text dark @click="dialog2=true">
-              {{ $t('actions.remove') }}
-            </v-btn>
-            <v-btn
-              large
-              dark
-              text
-              @click="saveServer"
-            >
-              {{ $t('actions.save') }}
-            </v-btn>
-          </v-toolbar-items>
+          <v-btn
+            color="green"
+            @click="newServer"
+          >
+            {{ $t('actions.new_server') }}
+          </v-btn>
         </v-toolbar>
-        <v-card-text>
-          <v-form
-            ref="form"
-            v-model="valid"
-          >
-            <v-text-field
-              v-model="serverName"
-              :label="$t('fields.server_name')"
-              :rules="rules.name"
-              autocomplete="new-password"
-            />
-            <v-text-field
-              v-model="serverIp"
-              :label="$t('fields.server_ip')"
-              :rules="rules.ip"
-              autocomplete="new-password"
-            />
-            <v-text-field
-              v-model="serverPort"
-              :label="$t('fields.rcon_port')"
-              :rules="rules.port"
-              autocomplete="new-password"
-              type="number"
-            />
-            <v-text-field
-              v-model="serverPassword"
-              autocomplete="new-password"
-              :label="$t('fields.rcon_password')"
-              :rules="rules.password"
-              :type="showPassword ? 'text' : 'password'"
-              :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
-              @click:append="showPassword = !showPassword"
-            />
-          </v-form>
-        </v-card-text>
-      </v-card>
-    </v-dialog>
-    <strong>
-      {{ $t('titles.servers') }}
-    </strong>
-    <v-row class="mt-3">
-      <v-col
-        v-for="server in serversList"
-        :key="server.serverId"
-        cols="12"
-        lg="4"
-        md="6"
-      >
-        <v-card
-          min-height="170"
-          text
-          class="mb-2"
-          elevation="10"
-          @click="serverDialog(server)"
+      </template>
+      <template #[`item.actions`]="{ item }">
+        <v-dialog
+          v-model="dialog"
+          max-width="500"
         >
-          <v-list-item three-line>
-            <v-list-item-content>
-              <v-list-item-title class="text-h4 mb-3">
-                {{ server.serverName }}
-              </v-list-item-title>
-              <v-list-item-subtitle>
-                <v-chip>
-                  <v-icon small>
-                    mdi-server
-                  </v-icon>{{ server.serverIp }}:{{ server.serverPort }}
-                </v-chip>
-              </v-list-item-subtitle>
-            </v-list-item-content>
-          </v-list-item>
-        </v-card>
-      </v-col>
-    </v-row>
-    <v-btn
-      large
-      block
-      text
-      color="blue"
-      class="mt-5"
-      @click="newServer"
-    >
-      {{ $t('actions.new_server') }}
-    </v-btn>
-    <v-dialog
-      v-model="dialog2"
-      max-width="400"
-    >
-      <v-card>
-        <v-card-title class="text-h5">
-          {{ $t('titles.are_you_sure') }}
-        </v-card-title>
-        <v-card-text>
-          {{ $t('misc.after_server_delete') }}
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="green darken-1"
-            text
-            @click="dialog2 = false"
-          >
-            {{ $t('actions.cancel') }}
-          </v-btn>
-          <v-btn
-            color="red"
-            text
-            @click="removeServer"
-          >
-            {{ $t('actions.remove') }}
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
+          <template #activator="{ on, attrs }">
+            <v-icon
+              small
+              class="mr-2"
+              v-bind="attrs"
+              @click="applyServer(item)"
+              v-on="on"
+            >
+              mdi-pencil
+            </v-icon>
+          </template>
+          <v-card tile flat>
+            <v-toolbar
+              max-height="80"
+              dark
+              color="primary"
+              class="mb-4"
+            >
+              <v-btn
+                icon
+                dark
+                @click="dialog = false"
+              >
+                <v-icon>mdi-close</v-icon>
+              </v-btn>
+              <v-toolbar-title>
+                <span class="text-h5">{{ $t('titles.server_config') }}</span>
+              </v-toolbar-title>
+              <v-spacer />
+              <v-toolbar-items>
+                <v-btn
+                  large
+                  dark
+                  text
+                  @click="saveServer"
+                >
+                  {{ $t('actions.save') }}
+                </v-btn>
+              </v-toolbar-items>
+            </v-toolbar>
+            <v-card-text>
+              <v-form
+                ref="form"
+                v-model="valid"
+              >
+                <v-text-field
+                  v-model="serverName"
+                  :label="$t('fields.server_name')"
+                  :rules="rules.name"
+                  autocomplete="new-password"
+                />
+                <v-text-field
+                  v-model="serverIp"
+                  :label="$t('fields.server_ip')"
+                  :rules="rules.ip"
+                  autocomplete="new-password"
+                />
+                <v-text-field
+                  v-model="serverPort"
+                  :label="$t('fields.rcon_port')"
+                  :rules="rules.port"
+                  autocomplete="new-password"
+                  type="number"
+                />
+                <v-text-field
+                  v-model="serverPassword"
+                  autocomplete="new-password"
+                  :label="$t('fields.rcon_password')"
+                  :rules="rules.password"
+                  :type="showPassword ? 'text' : 'password'"
+                  :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+                  @click:append="showPassword = !showPassword"
+                />
+              </v-form>
+            </v-card-text>
+          </v-card>
+        </v-dialog>
+        <v-dialog
+          v-model="dialog2"
+          max-width="400"
+        >
+          <template #activator="{ on, attrs }">
+            <v-icon
+              small
+              v-bind="attrs"
+              v-on="on"
+            >
+              mdi-delete
+            </v-icon>
+          </template>
+          <v-card>
+            <v-card-title class="text-h5">
+              {{ $t('titles.are_you_sure') }}
+            </v-card-title>
+            <v-card-text>
+              {{ $t('misc.after_server_delete') }}
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer />
+              <v-btn
+                color="green darken-1"
+                text
+                @click="dialog2 = false"
+              >
+                {{ $t('actions.cancel') }}
+              </v-btn>
+              <v-btn
+                color="red"
+                text
+                @click="removeServer(item)"
+              >
+                {{ $t('actions.remove') }}
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+      </template>
+    </v-data-table>
   </div>
 </template>
 <script>
@@ -163,6 +161,22 @@ export default {
   },
   data () {
     return {
+      data_headers: [
+        {
+          text: this.$t('fields.server_name'),
+          align: 'start',
+          value: 'serverName'
+        },
+        {
+          text: this.$t('fields.server_ip'),
+          value: 'serverIp'
+        },
+        {
+          text: this.$t('fields.actions'),
+          value: 'actions',
+          sortable: false
+        }
+      ],
       dialog2: false,
       dialog: false,
       valid: false,
@@ -209,13 +223,12 @@ export default {
     }
   },
   methods: {
-    serverDialog (server) {
+    applyServer (server) {
       this.serverId = server.serverId
       this.serverPort = server.serverPort
       this.serverName = server.serverName
       this.serverIp = server.serverIp
       this.serverPassword = server.serverPassword
-      this.dialog = true
     },
     saveServer () {
       this.$refs.form.validate()
@@ -233,9 +246,9 @@ export default {
         this.dialog = false
       }
     },
-    removeServer () {
+    removeServer (server) {
       const { shopid } = this.$route.params
-      const { serverId } = this
+      const { serverId } = server
       this.$fire.database.ref().child(`shops/${shopid}/servers/${serverId}`).remove()
       this.$fire.database.ref().child(`servers/${serverId}`).remove()
       this.dialog = false
