a4fc5679ac609f7451bf1d5c1fd52b25c63ac51a
Michał Jażdżyk <michal.jazdzyk.007@gmail.com>
2022-03-29 21:02:55 +0200

    add esm module

--- nuxt.config.js
+++ nuxt.config.js
@@ -181,12 +181,7 @@ export default {
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
     transpile: ['vuetify/lib', 'tiptap-vuetify'],
-    extractCSS: true,
-    extend (config, {}) {
-      config.node = {
-        fs: 'empty'
-      }
-    }
+    extractCSS: true
   },
   serverMiddleware: process.env.VERCEL || process.env.NETLIFY || process.env.NETLIFY_DEV ? [] : [
     '~/api/rcon.js',
