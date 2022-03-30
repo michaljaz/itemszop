38827eb286b63e64ce6b4404529334f034123162
Michał Jażdżyk <michal.jazdzyk.007@gmail.com>
2022-03-30 10:42:21 +0200

    update rules in module

---
+++ modules/update_firebase_rules.js
@@ -0,0 +1,25 @@
+import * as admin from 'firebase-admin'
+import fs from 'fs'
+
+// update firebase rules
+export default function () {
+	const {serviceAccount, databaseURL} = JSON.parse(process.env.FIREBASE_CONFIG)
+	if (admin.apps.length === 0) {
+	  admin.initializeApp({
+	    credential: admin.credential.cert(serviceAccount),
+	    databaseURL
+	  })
+	  let rules
+	  if(process.env.OWNER_ID){
+	    rules = fs.readFileSync('./misc/one_owner_firebase.rules.json', 'utf-8').replace(/'OWNER_ID'/g,`'${process.env.OWNER_ID}'`)
+	  }else{
+	    rules = fs.readFileSync('./misc/firebase.rules.json', 'utf-8')
+	  }
+	  admin.database().setRules(rules).then(()=>{
+			console.log('Rules updated successfully')
+		}).catch((e)=>{
+			console.log(e)
+		})
+	}
+
+}
