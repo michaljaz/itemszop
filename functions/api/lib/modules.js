6325291c0a0072ada9a655998efe5ec7feb264bc
Michał Jażdżyk <michal.jazdzyk.007@gmail.com>
2022-03-31 19:01:44 +0200

    fix netlify apiurl

--- functions/api/lib/modules.js
+++ functions/api/lib/modules.js
@@ -8,7 +8,7 @@ const cors = require('cors')
 app.use(cors())
 
 const baseUrl = process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : process.env.URL
-const apiUrl = (process.env.NETLIFY && process.env.NETLIFY_DEV) ? `${baseUrl}/.netlify/functions` : `${baseUrl}/api`
+const apiUrl = (process.env.NETLIFY || process.env.NETLIFY_DEV) ? `${baseUrl}/.netlify/functions` : `${baseUrl}/api`
 
 // REQUEST
 
@@ -29,18 +29,6 @@ exports.request = (handler, filename) => {
       statusCode: 200,
       body: JSON.stringify({success: false, error})
     }))
-  }
-  app.onRequest = async (context) => {
-    // Contents of context object
-    const {
-      request, // same as existing Worker API
-      env, // same as existing Worker API
-      params, // if filename includes [id] or [[path]]
-      waitUntil, // same as ctx.waitUntil in existing Worker API
-      next, // used for middleware or to fetch assets
-      data // arbitrary space for passing data between middlewares
-    } = context
-    return new Response("HELLO WORLD")
   }
   return app
 }
