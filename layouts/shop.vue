f53bf8713cafb09ac222895752b7bd9edfa22fac
Wiktor Józwiak <61411400+ReferTV@users.noreply.github.com>
2022-03-31 21:16:30 +0200

    Update shop.vue

--- layouts/shop.vue
+++ layouts/shop.vue
@@ -47,14 +47,19 @@
       <div class="mt-3">
         <v-row>
           <v-col md="8" sm="12" xs="12" cols="12">
-            <v-alert v-if="shop.announcement" border="left" colored-border :color="shop.theme ? shop.theme : 'blue darken-4'">
-              <!-- eslint-disable vue/no-v-html -->
-              <div
-                style="max-height: 200px"
-                class="mt-2 overflow-y-auto"
-                v-html="shop.announcement"
-              />
-              <!--eslint-enable-->
+          <v-alert
+             v-if="shop.announcement"
+              color="green"
+              dismissible
+              outlined
+              prominent
+              text
+              type="info"
+              >
+        <div
+            class="mt-2 overflow-y-auto"
+            v-html="shop.announcement"
+         />
             </v-alert>
             <v-card>
               <v-card-text>
