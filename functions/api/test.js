4ed6a54099292f2f7383a3ba697c7f839afedbb1
Michał Jażdżyk <michal.jazdzyk.007@gmail.com>
2022-04-03 16:09:24 +0200

    rcon deno driver cloudflare test

--- functions/api/test.js
+++ functions/api/test.js
@@ -1,35 +1,35 @@
 const { getTokenFromGCPServiceAccount } = require('@sagi.io/workers-jwt')
+import { RCON } from './driver/rcon/index.js'
 
-async function getAccessToken (firebaseConfig) {
-  const jwtToken = await getTokenFromGCPServiceAccount({
-    serviceAccountJSON: JSON.parse(firebaseConfig).serviceAccount,
-    aud: 'https://oauth2.googleapis.com/token',
-    payloadAdditions: {
-      scope: [
-        // scope required for firestore
-        'https://www.googleapis.com/auth/datastore',
-        // The following scopes are required only for realtime database
-        'https://www.googleapis.com/auth/userinfo.email',
-        'https://www.googleapis.com/auth/firebase.database'
-      ].join(' ')
-    }
-  })
 
-  const accessToken = await (
-    await fetch('https://oauth2.googleapis.com/token', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded'
-      },
-      body: new URLSearchParams({
-        grant_type: 'urn:ietf:params:oauth:grant-type:jwt-bearer',
-        assertion: jwtToken // the JWT token generated in the previous step
-      })
-    })
-  ).json()
-
-  return accessToken.access_token
-}
+// async function getAccessToken (firebaseConfig) {
+//   const jwtToken = await getTokenFromGCPServiceAccount({
+//     serviceAccountJSON: JSON.parse(firebaseConfig).serviceAccount,
+//     aud: 'https://oauth2.googleapis.com/token',
+//     payloadAdditions: {
+//       scope: [
+//         // The following scopes are required only for realtime database
+//         'https://www.googleapis.com/auth/userinfo.email',
+//         'https://www.googleapis.com/auth/firebase.database'
+//       ].join(' ')
+//     }
+//   })
+//
+//   const accessToken = await (
+//     await fetch('https://oauth2.googleapis.com/token', {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/x-www-form-urlencoded'
+//       },
+//       body: new URLSearchParams({
+//         grant_type: 'urn:ietf:params:oauth:grant-type:jwt-bearer',
+//         assertion: jwtToken // the JWT token generated in the previous step
+//       })
+//     })
+//   ).json()
+//
+//   return accessToken.access_token
+// }
 
 export async function onRequest (context) {
   // Contents of context object
@@ -42,17 +42,30 @@ export async function onRequest (context) {
     data // arbitrary space for passing data between middlewares
   } = context
 
-  const {databaseURL} = JSON.parse(env.FIREBASE_CONFIG).publicConfig
-  const token = await getAccessToken(env.FIREBASE_CONFIG)
+  const rcon = new RCON()
+  // const {databaseURL} = JSON.parse(env.FIREBASE_CONFIG).publicConfig
+  // const token = await getAccessToken(env.FIREBASE_CONFIG)
+  //
+  // const result = await (
+  //   await fetch(`${databaseURL}/shops.json`, {
+  //     method: 'GET',
+  //     headers: {
+  //       'Authorization': `Bearer ${token}`
+  //     }
+  //   })
+  // ).json()
+  // Rcon.connect({host:"localhost", port:25575, password:'password'}).then((rcon) => {
+  //   rcon.send('list').then((response) => {
+  //     count++
+  //     if (count === commands.length) {
+  //       console.log(response)
+  //     }
+  //   }).catch((e) => {
+  //     console.log('command_error')
+  //   })
+  // }).catch((e) => {
+  //   console.log(e)
+  // })
 
-  const result = await (
-    await fetch(`${databaseURL}/shops.json`, {
-      method: 'GET',
-      headers: {
-        'Authorization': `Bearer ${token}`
-      }
-    })
-  ).json()
-
-  return new Response(JSON.stringify(result))
+  return new Response('xd')
 }
