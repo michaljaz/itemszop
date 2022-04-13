98fe08cb2bc6d247e37c2c9d0532e5a30a32e3f2
Michał Jażdżyk <michal.jazdzyk.007@gmail.com>
2022-04-13 17:43:15 +0200

    wrangler as package script

--- wrangler.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -a
-source <(cat .env | \
-    sed -e '/^#/d;/^\s*$/d' -e "s/'/'\\\''/g" -e "s/=\(.*\)/='\1'/g")
-set +a
-
-npx wrangler pages dev ./dist/ --binding FIREBASE_CONFIG="$FIREBASE_CONFIG"
