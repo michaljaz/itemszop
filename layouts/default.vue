87209f13e4cf9c069065063750abae66f42c3a24
Michał Jażdżyk <michal.jazdzyk.007@gmail.com>
2022-07-09 15:32:49 +0200

    mojang in shop footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -8,6 +8,9 @@
       <span>
         &copy; {{ new Date().getFullYear() }}
         <a :href="mainUrl">ItemSzop</a>
+        <template v-if="shopName">
+          | {{ shopName }} nie jest w żaden sposób powiązany z firmą Mojang AB.
+        </template>
       </span>
     </v-footer>
   </v-app>
@@ -18,16 +21,51 @@ export default {
   name: 'DefaultLayout',
   data () {
     return {
-      mainUrl: process.env.mainUrl
+      mainUrl: process.env.mainUrl,
+      oldShopId: false,
+      shopName: false
     }
   },
   computed: {
+    shopId () {
+      if (process.env.SINGLE_SHOP) {
+        return process.env.SINGLE_SHOP
+      } else if (this.$route.params.shopid) {
+        return this.$route.params.shopid
+      } else {
+        return false
+      }
+    },
     inframe () {
       try {
         return window.self !== window.top
       } catch (e) {
         return true
       }
+    }
+  },
+  watch: {
+    shopId () {
+      this.updateShopNameListener()
+    }
+  },
+  mounted () {
+    this.updateShopNameListener()
+  },
+  methods: {
+    updateShopNameListener () {
+      console.log(this.shopId)
+      if (this.oldShopId) {
+        this.$fire.database.ref().child(`shops/${this.oldShopId}/name`).off('value')
+      }
+      if (this.shopId) {
+        this.$fire.database.ref().child(`shops/${this.shopId}/name`).on('value', (snapshot) => {
+          this.shopName = snapshot.val()
+        })
+      } else {
+        this.shopName = false
+      }
+      this.oldShopId = this.shopId
     }
   }
 }
